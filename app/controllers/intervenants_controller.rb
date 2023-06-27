a03883c36a77cdac904b93aba8ed75c33f740243
VincentBeaumont <beaumontvincent@yahoo.fr>
2023-06-27 16:33:42 +0200

    back_ok

--- app/controllers/intervenants_controller.rb
+++ app/controllers/intervenants_controller.rb
@@ -14,16 +14,37 @@ class IntervenantsController < ApplicationController
   def create
     @intervenant = Intervenant.new(intervenant_params)
     if @intervenant.save
-      redirect_to intervenant_path(@intervenant)
+      redirect_to intervenants_path
     else
       render :new
     end
   end
 
+  def edit
+    @intervenant = Intervenant.find(params[:id])
+  end
+
+  def update
+    @intervenant = Intervenant.find(params[:id])
+    if @intervenant.update(intervenant_params)
+      redirect_to intervenant_path(@intervenant)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @intervenant = Intervenant.find(params[:id])
+    @intervenant.destroy
+    redirect_to intervenants_path, status: :see_other
+  end
+
+
+
   private
 
   def intervenant_params
-    params.require(:intervenant).permit(:prenom, :nom, :email, :adress, :code_postal, :ville,
+    params.require(:intervenant).permit(:prenom, :nom, :email, :adresse, :code_postal, :ville,
                                         :telephone, :num_da, :siret, :tarif, :photo)
   end
 
--- app/models/intervenant.rb
+++ app/models/intervenant.rb
@@ -1,6 +1,6 @@
 class Intervenant < ApplicationRecord
   has_many :sessions
-  validates :prenom, :nom, :email, :adress, :code_postal, :ville,
+  validates :prenom, :nom, :email, :adresse, :code_postal, :ville,
             :telephone, presence: true
   validates :prenom, :nom, uniqueness: true
   has_one_attached :photo
