9564db44ca450357e33b1cc431203106a2bda0bc
Hugo Vetrano <hugo.vetrano@ieseg.fr>
2023-06-30 01:10:51 +0200

    controller fixed

--- app/javascript/controllers/mesdonnees_search_bar_controller.js
+++ app/javascript/controllers/mesdonnees_search_bar_controller.js
@@ -2,7 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ['row'];
-
+  connect() {
+    console.log('Hello from MesdonneesSearchBarController');
+  }
   filterTable() {
     const searchTerm = this.element.querySelector('input').value.toLowerCase();
     console.log(searchTerm);
@@ -10,5 +12,12 @@ export default class extends Controller {
       const rowText = Array.from(row.querySelectorAll('td')).map(td => td.textContent.toLowerCase()).join(' ');
       row.classList.toggle('hidden', !rowText.includes(searchTerm));
     });
+  }
+
+  clicked(event) {
+    var link = event.currentTarget.dataset.link;
+    if (link) {
+      window.location.href = link;
+    }
   }
 }
