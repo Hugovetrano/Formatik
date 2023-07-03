083b1207857bbb5c7495b4d7ae912b281b09ab88
VincentBeaumont <beaumontvincent@yahoo.fr>
2023-07-04 00:48:33 +0200

    annuaire_specialite

--- app/javascript/controllers/new_session_form_controller.js
+++ app/javascript/controllers/new_session_form_controller.js
@@ -2,7 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-session-form"
 export default class extends Controller {
-  static targets = ['price', 'name', 'startDate', 'endDate']
+  static targets = ['price', 'name', 'startDate', 'endDate', 'rowspecialty', 'outputSpecialty']
+
+  connect() {
+    console.log('Hello from NewSessionFormController');
+  }
 
   updateInputs(event) {
     // Recuperation du prix du programme depuis une method du controller
@@ -18,4 +22,28 @@ export default class extends Controller {
       });
       // Assignation du nouveau prix à l'input
   }
+
+  keywordSearch(event) {
+    const searchWord = event.target.value.toLowerCase();
+    // const searchWord = this.element.querySelector('input').value.toLowerCase();
+    // console.log(searchWord);
+    this.rowspecialtyTargets.forEach((rowspecialty) => {
+      const rowText = Array.from(rowspecialty.querySelectorAll('td')).map(td => td.textContent.toLowerCase()).join(' ');
+      rowspecialty.classList.toggle('hidden', !rowText.includes(searchWord));
+    });
+  }
+
+  clicked(event) {
+    // ON RECUPERE LES DONNES DE L'ANNUAIRE DOM
+    const interv = event.target.parentNode
+    console.log(interv);
+    const firstColumn = interv.querySelector('td:first-child');
+    const sndColumn = interv.querySelector('td:nth-child(2)');
+    console.log(sndColumn);
+    const outputSpecialty = this.outputSpecialtyTarget;
+    console.log(outputSpecialty)
+    if (firstColumn) {
+        outputSpecialty.value = `${firstColumn.textContent} ${sndColumn.textContent}`;
+      }
+}
 }
