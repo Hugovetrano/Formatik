adc2ecafc6fd909a3bec7a936c88ec457cb3a488
VincentBeaumont <beaumontvincent@yahoo.fr>
2023-07-06 13:40:13 +0200

    l

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def dashboard
+    @session = Session.where(Date.new("date_debut".to_i) > Time.now).sort_by(&:date_debut).first
+    @programme = @session.programme.titre
+    @intervenant = "#{@session.intervenant.prenom} #{@session.intervenant.nom}"
+
+    @inscriptions = @session.inscriptions
+    @non_participants = []
+    @inscriptions.each do |inscription|
+      p = inscription.parcoursadmin
+      @non_participants << inscription unless p.convocation_envoye
+    end
+
+    @participants_confirmes = @inscriptions - @non_participants
   end
 end
